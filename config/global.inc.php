a010b43756a4ec0e61941adb8406434ee4563d03
Troels <tkn@tkn-laptop.(none)>
2009-11-14 21:38:50 +0100

    Fixed include paths

--- config/global.inc.php
+++ config/global.inc.php
@@ -1,9 +1,14 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 set_include_path(
-  get_include_path()
+  '.'
   . PATH_SEPARATOR . dirname(dirname(__FILE__))
-  . PATH_SEPARATOR . dirname(dirname(__FILE__)).'/lib');
+  . PATH_SEPARATOR . dirname(dirname(__FILE__)).'/lib'
+  . PATH_SEPARATOR . dirname(dirname(__FILE__)).'/thirdparty/konstrukt/lib'
+  . PATH_SEPARATOR . dirname(dirname(__FILE__)).'/thirdparty/pdoext/lib'
+  . PATH_SEPARATOR . dirname(dirname(__FILE__)).'/thirdparty/bucket/lib'
+  . PATH_SEPARATOR . get_include_path()
+);
 
 require_once 'konstrukt/konstrukt.inc.php';
 set_error_handler('k_exceptions_error_handler');
