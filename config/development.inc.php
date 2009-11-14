74f0faecd510e4f027382f4c50b8bb0cdf7be5db
Troels <tkn@tkn-laptop.(none)>
2009-11-14 21:51:54 +0100

    fixed shebang line + local dbname

--- config/development.inc.php
+++ config/development.inc.php
@@ -5,11 +5,14 @@ require_once 'applicationfactory.php';
 require_once 'bucket.inc.php';
 date_default_timezone_set('Europe/Paris');
 
+$debug_log_path = dirname(dirname(__FILE__)).'/log/debug.log';
+$debug_enabled = true;
+
 function create_container() {
   $factory = new ApplicationFactory();
   $container = new bucket_Container($factory);
   $factory->template_dir = realpath(dirname(__FILE__) . '/../templates');
-  $factory->pdo_dsn = 'mysql:host=localhost;dbname=test';
+  $factory->pdo_dsn = 'mysql:host=localhost;dbname=pearhub';
   $factory->pdo_username = 'root';
   return $container;
 }
