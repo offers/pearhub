29d6a6ff12960c8d8a2c1f86150a6e1991bb0611
Troels <tkn@tkn-laptop.(none)>
2009-12-27 15:09:32 +0100

    Made sql logging similar to other logs

--- config/development.inc.php
+++ config/development.inc.php
@@ -5,8 +5,9 @@ require_once 'applicationfactory.php';
 require_once 'bucket.inc.php';
 date_default_timezone_set('Europe/Paris');
 
-$debug_log_path = dirname(dirname(__FILE__)).'/log/debug.log';
-$debug_enabled = false;
+//$debug_log_path = dirname(dirname(__FILE__)).'/log/debug.log';
+//$sql_log_path = dirname(dirname(__FILE__)).'/log/pdoext.log';
+//$debug_enabled = true;
 
 function create_container() {
   $factory = new ApplicationFactory();
@@ -14,7 +15,9 @@ function create_container() {
   $factory->template_dir = realpath(dirname(__FILE__) . '/../templates');
   $factory->pdo_dsn = 'mysql:host=localhost;dbname=pearhub';
   $factory->pdo_username = 'root';
-  // $factory->pdo_log_target = dirname(dirname(__FILE__)).'/log/pdoext.log'
+  if (isset($GLOBALS['sql_log_path'])) {
+    $factory->pdo_log_target = $GLOBALS['sql_log_path'];
+  }
   $container->registerImplementation('PDO', 'pdoext_Connection');
   $container->registerImplementation('k_DefaultNotAuthorizedComponent', 'NotAuthorizedComponent');
   $container->registerImplementation('k_IdentityLoader', 'CookieIdentityLoader');
