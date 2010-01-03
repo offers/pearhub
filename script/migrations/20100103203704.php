62747db6c12a8893b1427133bab154e2779dee23
Troels Knak-Nielsen <tkn@tkn-ubuntu.(none)>
2010-01-03 20:42:34 +0100

    Dropped report column

--- script/migrations/20100103203704.php
+++ script/migrations/20100103203704.php
@@ -3,9 +3,4 @@
 require_once(dirname(__FILE__) . '/../../config/global.inc.php');
 $container = create_container();
 $db = $container->create('PDO');
-$db->exec(
-    '
-alter table projects add column release_policy enum("manual", "auto") not null default "auto" after php_version
-'
-);
-
+$db->exec('alter table releases drop column report');
