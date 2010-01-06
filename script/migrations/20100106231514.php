136bf615bfbae614446521435fccc8d03a9548a7
Troels Knak-Nielsen <tkn@tkn-ubuntu.(none)>
2010-01-06 23:16:28 +0100

    Fixed schema for cache table

---
+++ script/migrations/20100106231514.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+require_once(dirname(__FILE__) . '/../../config/global.inc.php');
+$container = create_container();
+$db = $container->create('PDO');
+$db->exec('drop table repository_types');
+$db->exec(
+    '
+create table repository_types (
+  url varchar(255) not null,
+  type varchar(20) not null,
+  primary key (url),
+  index (url)
+)
+'
+);
