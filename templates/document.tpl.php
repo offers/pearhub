44c4af3833d50d80a5f92f893d168c3aaacdf085
Troels Knak-Nielsen <tkn@tkn-ubuntu.(none)>
2010-01-07 23:50:43 +0100

    Added uservoice feedback

--- templates/document.tpl.php
+++ templates/document.tpl.php
@@ -55,6 +55,30 @@ echo implode(' / ', $tmp);
       <?php echo $javascript; ?>
     </script>
 <?php endforeach; ?>
+<script type="text/javascript">
+var uservoiceOptions = {
+  /* required */
+  key: 'pearhub',
+  host: 'pearhub.uservoice.com',
+  forum: '37721',
+  showTab: true,
+  /* optional */
+  alignment: 'left',
+  background_color:'#808',
+  text_color: 'white',
+  hover_color: '#C28',
+  lang: 'en'
+};
+
+function _loadUserVoice() {
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
+  document.getElementsByTagName('head')[0].appendChild(s);
+}
+_loadSuper = window.onload;
+window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
+</script>
 <script type="text/javascript" src="http://include.reinvigorate.net/re_.js"></script>
 <script type="text/javascript">
 <?php if (!$context->identity()->anonymous()): ?>
