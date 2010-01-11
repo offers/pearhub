14d32efb31ed4a7ffdcdfb10a20092eb5a665b36
Troels Knak-Nielsen <tkn@tkn-ubuntu.(none)>
2010-01-11 16:12:55 +0100

    More optimisation of uservoice loading

--- templates/document.tpl.php
+++ templates/document.tpl.php
@@ -69,19 +69,8 @@ var uservoiceOptions = {
   hover_color: '#C28',
   lang: 'en'
 };
-
-function _loadUserVoice() {
-  var s = document.createElement('script');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
-  document.getElementsByTagName('head')[0].appendChild(s);
-}
-/*
-_loadSuper = window.onload;
-window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
-*/
-_loadUserVoice();
 </script>
+<script type="text/javascript" src="http://cdn.uservoice.com/javascripts/widgets/tab.js"></script>
 <script type="text/javascript" src="http://include.reinvigorate.net/re_.js"></script>
 <script type="text/javascript">
 <?php if (!$context->identity()->anonymous()): ?>
