6744b1a3bd70dc9b81f16c8e8c4fe789d5b9f11f
Troels Knak-Nielsen <tkn@tkn-ubuntu.(none)>
2010-01-11 16:06:25 +0100

    load uservoice directly

--- templates/document.tpl.php
+++ templates/document.tpl.php
@@ -76,8 +76,11 @@ function _loadUserVoice() {
   s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
   document.getElementsByTagName('head')[0].appendChild(s);
 }
+/*
 _loadSuper = window.onload;
 window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
+*/
+_loadUserVoice();
 </script>
 <script type="text/javascript" src="http://include.reinvigorate.net/re_.js"></script>
 <script type="text/javascript">
